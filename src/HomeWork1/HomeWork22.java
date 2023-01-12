d8eab1afd646660df0aad5953bdf482c10407c82
nejmessabah17 <nbouchra998@gmail.com>
2023-01-12 18:05:51 -0500

    v

---
+++ src/HomeWork1/HomeWork22.java
@@ -0,0 +1,10 @@
+package HomeWork1;
+
+public class HomeWork22 {
+    public static void main(String[] args) {
+        Boolean myBoolean=false;
+        double money=99999.99;
+        System.out.println();
+
+    }
+}
